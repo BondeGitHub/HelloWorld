f6a4fdc15ee85f7aaf1c6fa6fe400d3243aa142c
BondeGitHub <44276197+BondeGitHub@users.noreply.github.com>
2018-10-18 23:27:28 +0200

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+            Console.WriteLine("HelloWorld Line 2 added");
+            Console.WriteLine("HelloWorld Linje 3 tilføjet");
+            Console.ReadLine();
+        }
+    }
+}
